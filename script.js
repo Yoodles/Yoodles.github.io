829364454cddde63ff4d4b2842eac9b7f3dde22c
Yoyo342 <146973197+Yoyo342@users.noreply.github.com>
2024-10-25 19:03:29 +0900

    j

--- script.js
+++ script.js
@@ -303,7 +303,7 @@ function updateGame(action) {
     switch (action) {   
         case 'submit':
         case 'delete':
-            gameState.gamePhase = 'midRound';
+            // gameState.gamePhase = 'midRound';
             // updateUI(action);
         case 'flip':
             updateUI(action);
